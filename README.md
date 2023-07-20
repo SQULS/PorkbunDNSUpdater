039c7f0c85214c65127f35ebff40a8f0dca19923
kenny <kljeffery@gmail.com>
2023-07-21 00:33:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Porkbun DNS
+
+Service that checks current external IP and updates DNS records with Porkbun accordingly.
---
+++ pbDNSUpdater.js
@@ -0,0 +1,51 @@
+const ipUrl = 'https://api.ipify.org?format=json';
+const apiUrl = 'https://porkbun.com/api/json/v3/dns/edit/squls.art/ID';
+let externalIp = '';
+let failCount = 0;
+
+function runFetch() {
+    fetch(ipUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            const options = {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    "Content-Type": 'application/json;charset=UTF-8',
+                },
+                body: JSON.stringify({
+                    secretapikey: '',
+                    apikey: '',
+                    name: 'www',
+                    type: 'A',
+                    content: data.ip
+                }),
+            };
+
+            if (externalIp !== data.ip) {
+                externalIp = data.ip;
+                return fetch(apiUrl, options);
+            } else {
+                throw new Error('IP has not changed');
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.status == 'ERROR') {
+                if (failCount < 2) {
+                    clearInterval(process);
+                    runFetch();
+                    failCount++;
+                } else {
+                    clearInterval(process);
+                    console.log('Exiting due to error: ' + data.message);
+                    return;
+                }
+            }
+        })
+        .catch((error) => {
+        });
+}
+
+const process = setInterval(runFetch, 5000);
