4702df14354fb4ca37151c6553273dcda6006141
kenny <kljeffery@gmail.com>
2023-07-21 11:12:36 +0100

    Make public

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Porkbun DNS
 
 Service that checks current external IP and updates DNS records with Porkbun accordingly.
+
+In order to utilise the Fetch() API that this service uses you will need Node.js 19 or higher installed on your server.
--- pbDNSUpdater.js
+++ pbDNSUpdater.js
@@ -1,7 +1,6 @@
 const ipUrl = 'https://api.ipify.org?format=json';
 const apiUrl = 'https://porkbun.com/api/json/v3/dns/edit/DOMAIN/ID';
 let externalIp = '';
-let failCount = 0;
 
 function runFetch() {
     fetch(ipUrl)
@@ -14,37 +13,41 @@ function runFetch() {
                     "Content-Type": 'application/json;charset=UTF-8',
                 },
                 body: JSON.stringify({
-                    secretapikey: '',
-                    apikey: '',
+                    secretapikey: 'SECRET_API_KEY',
+                    apikey: 'API_KEY',
                     name: 'www',
                     type: 'A',
                     content: data.ip
                 }),
             };
 
-            if (externalIp !== data.ip) {
+            if (data && externalIp !== data.ip) {
                 externalIp = data.ip;
                 return fetch(apiUrl, options);
             } else {
-                throw new Error('IP has not changed');
+                throw new Error('Message: IP has not changed');
             }
         })
-        .then((response) => response.json())
+        .then((response) => {
+            // Additional error handling for response
+            if (!response || !response.json) {
+                throw new Error('Error: Invalid response or not a JSON object');
+            }
+            return response.json();
+        })
         .then((data) => {
-            console.log(data);
+
             if (data.status == 'ERROR') {
-                if (failCount < 2) {
-                    clearInterval(process);
-                    runFetch();
-                    failCount++;
-                } else {
-                    clearInterval(process);
-                    console.log('Exiting due to error: ' + data.message);
-                    return;
-                }
+                clearInterval(process);
+                runFetch();
+            } else {
+                console.log('Message: ' + data.status);
             }
+
         })
-        .catch((error) => {});
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
 
-const process = setInterval(runFetch, 5000);
+const process = setInterval(runFetch, 3600000 );
